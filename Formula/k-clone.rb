8d028027f7fb61dbb27b3aefaa14864235febdc2
nxk0122 <nxk0122@dexcom.com>
2026-05-04 12:55:22 -0700

    Experiment

--- Formula/k-clone.rb
+++ Formula/k-clone.rb
@@ -3,11 +3,10 @@ class KClone < Formula
   homepage "https://github.com/niteshkumarm287/k-clone"
   url "https://github.com/niteshkumarm287/k-clone/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "ca07a29c8d8aea99048ebe6534c4bb412e2d01b8f402d51a6c9b949ff220e390"
-  license "NA"
+  license "MIT"
   version "1.0.0"
 
-  depends_on "kubectl"
-  depends_on "jq"
+  uses_from_macos "zsh"
 
   def install
     bin.install "k-clone.zsh" => "k-clone"
